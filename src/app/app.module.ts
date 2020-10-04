4527ac3b7636899d12059a6cd9a3a834e84220df
melissagovender9701 <melissa.govender9701@gmail.com>
2020-10-04 23:00:41 +0200

    navbar & about

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './Components/navbar/navbar.component';
+import { AboutComponent } from './Components/about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
